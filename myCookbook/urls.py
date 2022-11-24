9a600680c90664e99b409fbc3a3d02cb9b74dee7
Nicole Mah <mnmah@alaska.edu>
2022-11-24 14:56:27 -0700

    Adding html pages

--- myCookbook/urls.py
+++ myCookbook/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("addRecipe", views.addRecipe, name="addRecipe"),
+    path("allRecipes", views.allRecipes, name="allRecipes"),
+    path("contactUs", views.contactUs, name="contactUs"),
+
 
 ]
--- myCookbook/views.py
+++ myCookbook/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-
     return render(request, "myCookbook/index.html")
+
+def addRecipe(request):
+    return render(request, "myCookbook/addRecipe.html")
+
+def allRecipes(request):
+    return render(request, "myCookbook/allRecipes.html")
+
+def contactUs(request):
+    return render(request, "myCookbook/contactUs.html")
