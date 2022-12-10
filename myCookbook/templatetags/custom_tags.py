a5f3fd72f84267f5da4b3319798b3efa6463a4b6
Nicole Mah <mnmah@alaska.edu>
2022-12-09 23:10:56 -0800

    new instructions

--- myCookbook/templatetags/custom_tags.py
+++ myCookbook/templatetags/custom_tags.py
@@ -3,28 +3,19 @@ from myCookbook import views
 
 register = template.Library()
 
-def newIngredients2(ingredients):
-    print(ingredients)
-    newIngredients =[]
-    
-    for i in ingredients:
-        str = ''
-        for item in i:
-            str = str + ' ' + item
-        str = str.lstrip(' ')
-        newIngredients.append(str)
-
-    return newIngredients
-    
-register.filter('newIngredients', newIngredients2)
-
 def newIngredients(ingredients):
-    print(ingredients)
     newIngredients =[]
     new = []
     newIngredients = ingredients.split(":")
     newIngredients = views.grouper(3,newIngredients)
-    print(newIngredients)
     return newIngredients
     
 register.filter('newIngredients', newIngredients)
+
+def newInstructions(instructions):
+    print(instructions)
+    newInstructions = instructions.replace(';',',').split(':')
+    print(newInstructions)
+    return newInstructions
+
+register.filter('newInstructions',newInstructions)
